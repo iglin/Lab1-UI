f5d8cc215eb4ac15d38c819e4e4d2be365d054b3
iglin <iglin@github.com>
2016-11-06 13:20:05 +0400

    blank fragment

--- app/src/main/java/com/example/alexander/lab1_ui/MainActivity.java
+++ app/src/main/java/com/example/alexander/lab1_ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexander.lab1_ui;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,8 +14,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.alexander.lab1_ui.fragments.NewNoteFragment;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, NewNoteFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,5 +94,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+// TODO: WTF IS THAT?! FIGURE OUT
     }
 }
