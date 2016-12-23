c350aab144cb017fe06c9685fdf77a03f667f16c
iglin <iglin@github.com>
2016-12-24 01:04:28 +0400

    default settings activity

--- app/src/main/java/com/example/alexander/lab1_ui/MainActivity.java
+++ app/src/main/java/com/example/alexander/lab1_ui/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.alexander.lab1_ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -93,7 +91,11 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_tags) {
 
         } else if (id == R.id.nav_settings) {
-
+            Intent intent = new Intent(this, SettingsActivity.class);
+         //   EditText editText = (EditText) findViewById(R.id.edit_message);
+         //   String message = editText.getText().toString();
+         //  intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
